b9bae050a55505259ceee9ec73f77cc15941536c
İlker Ünver <ilkerunver96@gmail.com>
2026-06-09 22:16:12 +0300

    Use DATABASE_URL for Alembic migrations

--- alembic/env.py
+++ alembic/env.py
@@ -1,3 +1,5 @@
+import os
+
 from logging.config import fileConfig
 
 from sqlalchemy import pool
@@ -11,6 +13,10 @@ from app.models import Item, Order
 
 
 config = context.config
+
+database_url = os.getenv("DATABASE_URL")
+if database_url:
+    config.set_main_option("sqlalchemy.url", database_url)
 
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
